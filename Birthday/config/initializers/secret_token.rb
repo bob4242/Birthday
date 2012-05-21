a478121e3f682d890cb155304d645efc4d0ba547
Rob <rob@bluefields.com>
2012-05-21 15:42:57 +0000

    Inital Rails isntall

---
+++ Birthday/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birthday::Application.config.secret_token = '293ed2f751cbe7017484d8e7f01775e61c0a2a6ae12dbc5a991c77b5ce174c1124940a56c7241f4ad805dce269e91d8e0be926b2b1fc1ebf74faeab8fcd88472'
